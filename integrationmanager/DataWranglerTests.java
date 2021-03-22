25a76ea96181cc4218aa13ac378be3fe843dd87c
Kaidenschears <Kaidenschears@users.noreply.github.com>
2021-03-22 18:06:48 -0500

    Update DataWranglerTests.java

--- integrationmanager/DataWranglerTests.java
+++ integrationmanager/DataWranglerTests.java
@@ -25,7 +25,7 @@ public class DataWranglerTests {
  */
     public boolean testEmptyCSV(){
         tester = new DataReaderImplementation();
-        List<Event> eventsList;
+        List<EventInterface> eventsList;
 
         try{
             eventsList = tester.readDataSet(new StringReader(""));
@@ -49,7 +49,7 @@ public class DataWranglerTests {
      */
     public boolean testOneEvent(){
         tester = new DataReaderImplementation();
-        List<Event> eventsList;
+        List<EventInterface> eventsList;
 
         try{
             eventsList = tester.readDataSet(new StringReader(
@@ -74,7 +74,7 @@ public class DataWranglerTests {
      */
     public boolean testThreeEvents(){
         tester = new DataReaderImplementation();
-        List<Event> eventsList;
+        List<EventInterface> eventsList;
 
         try{
             eventsList = tester.readDataSet(new StringReader(
@@ -105,7 +105,7 @@ public class DataWranglerTests {
     public boolean testEvent(){
         try{
             Date dateCheck = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("2021-Mar-19 14:00:00");
-            EventObject event = new EventObject("Science Exploration", "2021-Mar-19 14:00:00", "Liquid", "Bill Nye", "Make Science Fun");
+            Event event = new EventObject("Science Exploration", dateCheck, "Liquid", "Bill Nye", "Make Science Fun");
             if(!event.getGroupName().equals("Bill Nye")){
               System.out.println("check3");
               return false;
@@ -148,7 +148,8 @@ public class DataWranglerTests {
  * expected.
  */
     public boolean testToString(){
-        EventObject event = new EventObject("Science Exploration", "2021-Mar-19 14:00:00", "Liquid", "Bill Nye", "Make Science Fun");
+        Date dateCheck = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse("2021-Mar-19 14:00:00");
+        Event event = new Event("Science Exploration", dateCheck, "Liquid", "Bill Nye", "Make Science Fun");
         String check = event.toString();
         if(check.equals("Science Exploration with Bill Nye, Sun Sep 10 14:00:00 CST 24"))
             return true;
