fc32eeab75a2fe763eb16506554500c4c55addf5
KaidenSchears <kgschears@wisc.edu>
2021-03-22 21:54:19 +0000

    added all files to integration folder

---
+++ integrationmanager/FrontEndDeveloperTests.java
@@ -0,0 +1,211 @@
+// --== CS400 File Header Information ==--
+// Name: Ji Lau
+// Email: jlau24@wisc.edu
+// Team: DA: Red
+// Role: Front End Developer
+// TA: Dan Kiel
+// Lecturer: Gary Dahl
+// Notes to Grader: N/A
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.concurrent.TimeUnit;
+
+import org.hamcrest.CoreMatchers;
+import static org.hamcrest.CoreMatchers.not;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.Assert.*;
+
+public class FrontEndDeveloperTests {
+    private BackendInterface backend;
+
+    @BeforeEach
+    public void setUp() {
+        backend = new BackendDummy();
+    }
+    
+    /**
+     * Tests to see if the application will quit if the user inputs 'x.' This test will timeout after 15 seconds - if a timeout occurs, the application did not exit and
+     *      the test fails.
+     */
+    @Test
+    @Timeout(value = 15, unit = TimeUnit.SECONDS)
+    public void testXToQuitApplicationFromMainMode() {
+        PrintStream standardOut = System.out;
+		InputStream standardIn = System.in;
+        String input = "x";
+        InputStream inputStreamSimulator = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStreamSimulator);
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+        Frontend frontend = new Frontend(backend);
+        frontend.run();
+        System.setOut(standardOut);
+        System.setIn(standardIn);
+    }
+    
+    /**
+     * Tests to see that the application will switch to planning mode by typing 'p'.
+     */
+    @Test
+    public void testPSwitchToPlanningMode() {
+        PrintStream standardOut = System.out;
+        InputStream standardIn = System.in;
+        String input = "p" + System.lineSeparator() + "x" + System.lineSeparator() + "x";
+        InputStream inputStreamSimulator = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStreamSimulator);
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+        Frontend frontend = new Frontend(backend);
+        frontend.run();
+
+        System.setOut(standardOut);
+        System.setIn(standardIn);
+        
+        String appOutput = outputStreamCaptor.toString(); 
+        assertThat(appOutput, CoreMatchers.containsString("===================PLANNING MODE===================="));
+        assertThat(appOutput, CoreMatchers.containsString("Type '<index>' to scroll to a certain index."));
+        assertThat(appOutput, CoreMatchers.containsString("Type 'p <index>' to pick an available time by its index."));
+        assertThat(appOutput, CoreMatchers.containsString("Type 'x' to return to the Main Mode."));
+    }
+
+    /**
+     * Tests to see that that after selecting a date that there are prompts for event name, description, group name, and venue.
+     */
+    @Test
+    public void testPlanningModePromptsUserForEventInfo() {
+        PrintStream standardOut = System.out;
+        InputStream standardIn = System.in;
+        String input = "";
+        input += "p" + System.lineSeparator(); 
+        input += "p 1" + System.lineSeparator(); 
+        input += "Mock Name" + System.lineSeparator();
+        input += "Mock Description" + System.lineSeparator();
+        input += "Mock Group Name" + System.lineSeparator();
+        input += "Mock Venue" + System.lineSeparator();
+        input += "Y" + System.lineSeparator();
+        input += "x" + System.lineSeparator();
+        input += "x";
+        InputStream inputStreamSimulator = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStreamSimulator);
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+        Frontend frontend = new Frontend(backend);
+        frontend.run();
+
+        System.setOut(standardOut);
+        System.setIn(standardIn);
+        
+        String appOutput = outputStreamCaptor.toString(); 
+        assertThat(appOutput, CoreMatchers.containsString("===================PLANNING MODE===================="));
+        assertThat(appOutput, CoreMatchers.containsString("You may type '!x' at any moment to cancel and return to the date selection screen."));
+        assertThat(appOutput, CoreMatchers.containsString("Event Name:"));
+        assertThat(appOutput, CoreMatchers.containsString("Event Description:"));
+        assertThat(appOutput, CoreMatchers.containsString("Group Name:"));
+        assertThat(appOutput, CoreMatchers.containsString("Venue:"));
+        assertThat(appOutput, CoreMatchers.containsString("You are about to create an event with the following details:"));
+        assertThat(appOutput, CoreMatchers.containsString("Event Name: Mock Name"));
+        assertThat(appOutput, CoreMatchers.containsString("Event Description: Mock Description"));
+        assertThat(appOutput, CoreMatchers.containsString("Group Name: Mock Group Name"));
+        assertThat(appOutput, CoreMatchers.containsString("Venue: Mock Venue"));
+        assertThat(appOutput, CoreMatchers.containsString("Do you wish to continue [Y/N]?:"));
+    }
+
+    /**
+     * Tests to see that the user will return back to date selection in planning mode if they type '!x'. Additionally, if any of the input prompts other than
+     *      event name (Event Description, etc.) appear, the test will fail.
+     */
+    @Test
+    public void testPlanningModeExitEventInput() {
+        PrintStream standardOut = System.out;
+        InputStream standardIn = System.in;
+        String input = "";
+        input += "p" + System.lineSeparator(); 
+        input += "p 1" + System.lineSeparator(); 
+        input += "!x" + System.lineSeparator(); 
+        input += "x" + System.lineSeparator();
+        input += "x";
+        InputStream inputStreamSimulator = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStreamSimulator);
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+        Frontend frontend = new Frontend(backend);
+        frontend.run();
+
+        System.setOut(standardOut);
+        System.setIn(standardIn);
+        
+        String appOutput = outputStreamCaptor.toString(); 
+        assertThat(appOutput, CoreMatchers.containsString("===================PLANNING MODE===================="));
+        assertThat(appOutput, CoreMatchers.containsString("You may type '!x' at any moment to cancel and return to the date selection screen."));
+        assertThat(appOutput, CoreMatchers.containsString("Event Name:"));
+        assertThat(appOutput, not(CoreMatchers.containsString("Event Description:")));
+        assertThat(appOutput, not(CoreMatchers.containsString("Group Name:")));
+        assertThat(appOutput, not(CoreMatchers.containsString("Venue:")));
+        assertThat(appOutput, not(CoreMatchers.containsString("You are about to create an event with the following details:")));
+        assertThat(appOutput, not(CoreMatchers.containsString("Do you wish to continue [Y/N]?:")));
+    }
+
+    /**
+     * Tests to see that the application will switch to searching mode when the user types 's'.
+     */
+    @Test
+    public void testSSwitchToSearchingMode() {
+        PrintStream standardOut = System.out;
+		InputStream standardIn = System.in;
+        String input = "";
+        input += "s" + System.lineSeparator();
+        input += "!x" + System.lineSeparator();
+        input += "x";
+        InputStream inputStreamSimulator = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStreamSimulator);
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+        Frontend frontend = new Frontend(backend);
+        frontend.run();
+
+        System.setOut(standardOut);
+        System.setIn(standardIn);
+        
+        String appOutput = outputStreamCaptor.toString(); 
+        assertThat(appOutput, CoreMatchers.containsString("=========SEARCHING MODE: DATE (MM/dd/yyyy)=========="));
+        assertThat(appOutput, CoreMatchers.containsString("Type '!n' to toggle searching by name."));
+        assertThat(appOutput, CoreMatchers.containsString("Type '!info <index>' to get more information about the event at the specified index."));
+        assertThat(appOutput, CoreMatchers.containsString("Type '!x' to return to the main mode."));
+    }
+
+    /**
+     * Tests to see that the application properly toggles between searching by name and searching by date while in searching mode when the user types '!n' and '!d'.
+     */
+    @Test
+    public void testSearchingModeToggleSearchByName() {
+        PrintStream standardOut = System.out;
+		InputStream standardIn = System.in;
+        String input = "";
+        input += "s" + System.lineSeparator(); 
+        input += "!n" + System.lineSeparator();
+        input += "!d" + System.lineSeparator();
+        input += "!x" + System.lineSeparator();
+        input += "x";
+        InputStream inputStreamSimulator = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStreamSimulator);
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+        Frontend frontend = new Frontend(backend);
+        frontend.run();
+
+        System.setOut(standardOut);
+        System.setIn(standardIn);
+        
+        String appOutput = outputStreamCaptor.toString(); 
+        assertThat(appOutput, CoreMatchers.containsString("=========SEARCHING MODE: DATE (MM/dd/yyyy)=========="));
+        assertThat(appOutput, CoreMatchers.containsString("================SEARCHING MODE: NAME================"));
+    }
+}
+
---
+++ integrationmanager/Frontend.java
@@ -0,0 +1,254 @@
+// --== CS400 File Header Information ==--
+// Name: Ji Lau
+// Email: jlau24@wisc.edu
+// Team: DA: Red
+// Role: Front End Developer
+// TA: Dan Kiel
+// Lecturer: Gary Dahl
+// Notes to Grader: N/A
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+class Frontend {
+    private BackendInterface backend;
+    private Scanner input;
+
+    public Frontend(BackendInterface backend) {
+        this.backend = backend;
+        input = new Scanner(System.in);
+    }
+    
+    public void run() {
+        List<EventInterface> events = backend.getThreeEvents(0);
+        int eventIndex = 0;
+        while (true) {
+            System.out.println();
+            System.out.println("==============Team DA: Venue Scheduler==============");
+            System.out.println("====================================================");
+            
+            if (events.size() > 0) {
+                System.out.println("Earliest scheduled events at this venue: ");
+                for (int i = 0; i < events.size(); i++) {
+                    EventInterface event = events.get(i);
+                    System.out.println(i+eventIndex+1 + ": " + event.getDate() + " - " +  event.getName());
+                }
+            } else {
+                System.out.println(("There are no events scheduled."));
+            }
+            System.out.println("====================================================");
+
+            System.out.println("Enter command: ");
+            if (events.size() > 0) {
+                System.out.println("Type '<index>' to scroll to a certain index.");
+            }
+            System.out.println("Type 'p' to enter Planning Mode.");
+            System.out.println("Type 's' to enter Searching Mode.");
+            System.out.println("Type 'x' to exit the Venue Scheduler.");
+            
+            String command = input.nextLine().trim().toLowerCase();
+
+            if (command.matches("^\\d+$")) {
+                int index = Integer.parseInt(command)-1;
+                if (index >= 0 && index <= backend.getNumberOfEvents()) {
+                    eventIndex = index;
+                    events = backend.getThreeEvents(eventIndex);
+                }
+            }
+            else if (command.equals("p")) {
+                runPlanningMode();
+                events = backend.getThreeEvents(0);
+            }
+            else if (command.equals("s")) {
+                runSearchingMode();
+                events = backend.getThreeEvents(0);
+            }
+            else if (command.equals("x")) {
+                break;
+            }
+            else {
+                System.out.println("Invalid command entered. Please try again.");
+            }
+        }
+    }
+
+    public void runPlanningMode() {
+        List<Date> availableTimes = backend.getThreeAvailableTimes(0);
+        int timeIndex = 0;
+        while (true) {
+            System.out.println();
+            System.out.println("===================PLANNING MODE====================");
+            System.out.println("====================================================");
+
+            if (availableTimes.size() > 0) {
+                System.out.println("Available Times:");
+                for (int i = 0; i < availableTimes.size(); i++) {
+                    Date event = availableTimes.get(i);
+                    System.out.println(i+timeIndex+1 + ": " + event.toString());
+                }
+            } else {
+                System.out.println("There are no available times.");
+            }
+            System.out.println("====================================================");
+
+            System.out.println("Type '<index>' to scroll to a certain index.");
+            System.out.println("Type 'p <index>' to pick an available time by its index.");
+            System.out.println("Type 'x' to return to the Main Mode.");
+            String command = input.nextLine().trim().toLowerCase();
+            if (command.matches("^\\d+$")) {
+                int index = Integer.parseInt(command)-1;
+                if (index >= 0 && index <= backend.getNumberAvailableTimes()) {
+                    timeIndex = index;
+                    availableTimes = backend.getThreeAvailableTimes(timeIndex);
+                }
+            }
+            else if (command.matches("^p \\d+\\s*$")) {
+                String[] parts = command.split(" ");
+                int index = Integer.parseInt(parts[1])-1;
+                List<Date> times = backend.getThreeAvailableTimes(index);
+                if (times.size() > 0) {
+                    promptUserForEventInfo(times.get(0));
+                }
+            }
+            else if (command.equals("x")) {
+                break;
+            } else {
+                System.out.println("You entered an invalid command.");
+            }
+            System.out.println();
+        }
+    }
+
+    public void promptUserForEventInfo(Date date) {
+        System.out.println();
+        System.out.println("===========PLANNING MODE: EVENT CREATION============");
+        System.out.println("====================================================");
+        System.out.println("You are currently entering data for the new event.");
+        System.out.println("You may type '!x' at any moment to cancel and return to the date selection screen.");
+
+        String[] prompts = {
+            "Event Name: ",
+            "Event Description: ",
+            "Group Name: ",
+            "Venue: "
+        };
+        List<String> answers = new ArrayList<String>();
+
+        for (int i = 0; i < prompts.length; i++) {
+            System.out.print(prompts[i]);
+            String answer = input.nextLine();
+            if (answer.trim().toLowerCase().equals("!x")) {
+                break;
+            }
+            answers.add(answer);
+        }
+
+        if (answers.size() == prompts.length) {
+            System.out.println("====================================================");
+            System.out.println("You are about to create an event with the following details:");
+            System.out.println("Event Date: " + date.toString());
+            for (int i = 0; i < answers.size(); i++) {
+                System.out.println(prompts[i] + answers.get(i));
+            }
+            System.out.println("Do you wish to continue [Y/N]?: ");
+
+            String answer = input.nextLine().trim().toLowerCase();
+            if (answer.equals("y")) {
+                backend.addEvent(date, answers.get(0), answers.get(1), answers.get(2), answers.get(3));
+            }
+        }
+    }
+
+    public void runSearchingMode() {
+        List<EventInterface> searchResults = new ArrayList<EventInterface>();
+        boolean searchByDate = true;
+        while (true) {
+            System.out.println();
+            if (searchByDate) {
+                System.out.println("=========SEARCHING MODE: DATE (MM/dd/yyyy)==========");
+            } else {
+                System.out.println("================SEARCHING MODE: NAME================");
+            }
+            System.out.println("====================================================");
+            if (searchResults.size() > 0) {
+                for (int i = 0; i < searchResults.size(); i++) {
+                    EventInterface event = searchResults.get(i);
+                    System.out.println((i+1) + ": " + event.getDate() + " - " + event.getName());
+                }
+            }
+            else {
+                System.out.println("No search results.");
+            }
+            System.out.println("====================================================");
+
+            if (searchByDate) {
+                System.out.println("Type a date in the format specified in the title (e.g: 01/01/2021) to search by date.");
+                System.out.println("Type '!n' to toggle searching by name.");
+            }
+            else {
+                System.out.println("Type the name of the event to search by name.");
+                System.out.println("Type '!d' to toggle searching by date.");
+            }
+            System.out.println("Type '!info <index>' to get more information about the event at the specified index.");
+            System.out.println("Type '!x' to return to the main mode.");
+
+            String command = input.nextLine().trim();
+            String commandLowerCase = command.toLowerCase();
+            if (commandLowerCase.equals("!d")) {
+                searchByDate = true;
+            }
+            else if (commandLowerCase.equals("!n")) {
+                searchByDate = false;
+            }
+            else if (commandLowerCase.matches("^!info \\d+\\s*$")) {
+                String[] parts = commandLowerCase.split(" ");
+                int index = Integer.parseInt(parts[1])-1;
+                if (index >= 0 && index < searchResults.size()) {
+                    displayEventInfo(searchResults.get(index));
+                }
+            }
+            else if (commandLowerCase.equals("!x")) {
+                break;
+            }
+            else if (searchByDate && command.matches("^\\d{2}/\\d{2}/\\d{4}\\s*$")) {
+                SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+                Date date = null;
+                try {
+                    date = formatter.parse(command);
+                }
+                catch (ParseException ex) {
+                    System.out.println("An unexpected parse exception occurred.");
+                }
+                searchResults = backend.getEventsByDate(date);
+            }
+            else if (!searchByDate) {
+                searchResults = backend.getEventsByName(command);
+            } else {
+                System.out.println("Invalid search input.");
+            }
+        }
+    }
+
+    public void displayEventInfo(EventInterface event) {
+        System.out.println();
+        System.out.println("===========SEARCHING MODE: EVENT EXPANSION==========");
+        System.out.println("====================================================");
+        System.out.println("Event Name: " + event.getName());
+        System.out.println("Event Date: " + event.getDate());
+        System.out.println("Event Description: " + event.getDescription());
+        System.out.println("Group Name: " + event.getGroupName());
+        System.out.println("Venue: " + event.getVenueSetting());
+        System.out.println("====================================================");
+        System.out.println("Enter anything to continue.");
+        input.nextLine();
+    }
+
+    public static void main(String[] args) {
+        Frontend frontend = new Frontend(new BackendDummy());
+        frontend.run();
+    }
+}
